2408085d84c4d2cb21360d1489cc5cff8aff110e
Thomas <roskewic@ualberta.ca>
2021-04-26 21:08:56 -0600

    Updated websever to restart on crash to limit downtime

--- start_webserver.sh
+++ start_webserver.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
 
-gunicorn --log-level=INFO dlapi.DLAPI:app -b 0.0.0.0:4248 -t 60
+trap "kill 0" INT
+
+while : 
+do
+    gunicorn --log-level=INFO dlapi.DLAPI:app -b 0.0.0.0:4248 -t 60
+    echo "DLAPI server has crashed! This could be casued by JDownloader not being started before the app"
+    echo "Waiting 10 and attempting it again..."
+    sleep 10
+done
