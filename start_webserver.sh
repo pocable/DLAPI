54f47d5ada4b1bf20500f8410aec4c71798d1a9b
Thomas <roskewic@ualberta.ca>
2021-04-29 13:29:11 -0600

    Updated timeout from 1 minute to 4 minutes

--- start_webserver.sh
+++ start_webserver.sh
@@ -4,7 +4,7 @@ trap "kill 0" INT
 
 while : 
 do
-    gunicorn --log-level=INFO dlapi:app -b 0.0.0.0:4248 -t 60
+    gunicorn --log-level=INFO dlapi:app -b 0.0.0.0:4248 -t 240
     echo "DLAPI server has crashed! This could be casued by JDownloader not being started before the app, or could be another cause."
     echo "Please open an issue if this happens a lot."
     echo "Waiting 10 seconds and attempting it again..."
